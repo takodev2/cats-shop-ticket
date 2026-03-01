8ed7ed5c56b76c825e61358702d4c27ea32a01cd
Tako <takosabu94@gmail.com>
2026-03-01 11:38:35 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-ADMIN_ROLE_ID = [int(rid) for rid in os.getenv("ADMIN_ROLE_IDS", "0").split(",") if rid]
-ADMIN_GET_ROLE = int(os.getenv("ADMIN_GET_ROLE", 0))
-TICKET_CATEGORY_ID = int(os.getenv("TICKET_CATEGORY_ID", 0))
-DONE_CATEGORY_ID = int(os.getenv("DONE_CATEGORY_ID", 0))
+LOG_CH_ID = 1313099999537532928
